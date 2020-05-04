abaace347f387c742de00816065ca34b966f8932
Brandon <bvasquez1994@gmail.com>
2020-05-04 12:47:33 -0400

    Initial commit

---
+++ EnemyAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    PlayerHealth target = null;
+    [SerializeField] float damage = 40f;
+
+
+    void Start()
+    {
+        target = FindObjectOfType<PlayerHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) { return; }
+        target.TakeDamage(damage);
+        Debug.Log("bang bang");
+    }
+
+    public void OnDamageTaken()
+    {
+        Debug.Log("I am working");
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera = null;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 20f;
+    [SerializeField] ParticleSystem muzzleFlashVFX = null;
+    [SerializeField] GameObject hitEffectVFX = null;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRayCast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlashVFX.Play();
+    }
+
+    private void ProcessRayCast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            CreateHitImpact(hit);
+            print(name + " hit" + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) { return; }
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        var hitEffect = Instantiate(hitEffectVFX, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(hitEffect, .1f);
+    }
+}
