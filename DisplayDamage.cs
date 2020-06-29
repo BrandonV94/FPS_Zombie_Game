d5296331d9830afb4762a4284f9f555119f2bc68
Brandon <bvasquez1994@gmail.com>
2020-06-29 17:50:28 -0400

    Created UI fedback for player damage.

---
+++ DisplayDamage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayDamage : MonoBehaviour
+{
+    [SerializeField] Canvas PlayerDamageCanvas = null;
+    [SerializeField] float impactTime = 1f;
+  
+    void Start()
+    {
+        PlayerDamageCanvas.enabled = false;
+    }
+
+    public void DisplayDamgeCanvas()
+    {
+        StartCoroutine(ToggleDamageCanvas());
+    }
+
+    IEnumerator ToggleDamageCanvas()
+    {
+        PlayerDamageCanvas.enabled = true;
+        yield return new WaitForSeconds(impactTime);
+        PlayerDamageCanvas.enabled = false;
+    }
+}
